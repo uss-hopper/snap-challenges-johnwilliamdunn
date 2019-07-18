7cf57ee623a30630df8707195c203427f81d5883
John Dunn <jdunn33@cnm.edu>
2019-07-18 10:36:30 -0600

    , added table for task snap challenge, added select statement for tweet. ,weight function snap challenge, adding pound calculation, added oz and kg calculation, added mg calculation, added return value, snap challenge solution.

--- snap-04-11/snap_7_18/snap1.js
+++ snap-04-11/snap_7_18/snap1.js
@@ -18,3 +18,15 @@ function(convertToGrams, weight, units); {
 
 
 }
+
+function sumUniquePositiveFactors(number) {
+	sum=0;
+	for(i=1, i<=number, i++){
+		if(number%i===0){
+			sum=sum+i;
+		}
+	}
+	return sum;
+}
+
+
