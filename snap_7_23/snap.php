1a5d6ac6ef2058cd287e6e1da24227ef26413451
John Dunn <jdunn33@cnm.edu>
2019-07-23 09:04:07 -0600

    , added table for task snap challenge, added select statement for tweet. ,weight function snap challenge, adding pound calculation, added oz and kg calculation, added mg calculation, added return value, snap challenge solution., snap challenge getter/setter method, writing method fixing errors

--- snap_7_23/snap.php
+++ snap_7_23/snap.php
@@ -33,3 +33,38 @@ class Coffee {
 $coffee = new Coffee("string", 123345);
 var_dump($coffee);
 echo $coffee->concat();
+
+Class Coffee {
+	private Coffee;
+	private Tea;
+
+	public function __construct( string $newCoffee, int $newTea) {
+		$this->setCoffee($newCoffee);
+		$this->setTea($newTea);
+		echo "NM Tea for me";
+	}
+
+	public function getCoffee() : string {
+		return $this->$coffee;
+	}
+
+	public function setCoffee(?string $newCoffee) : void {
+						$this->coffee = $newCoffee;
+	}
+
+	public function getTea() : int {
+		return $this->tea;
+	}
+
+	public function setTea($newTea) : void {
+		$this->tea = $newTea
+	}
+
+	public function concat() : string {
+		return $this->tea . (string)$this->tea;
+	}
+}
+
+$coffee = new Coffee("string" Folgers);
+var_dump($coffee);
+echo $coffee->concat();
