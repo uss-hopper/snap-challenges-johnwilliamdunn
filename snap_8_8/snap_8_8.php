78ffdf5f23612c6c8598297a71e82cb7a4afbf3b
John Dunn <jdunn33@cnm.edu>
2019-08-08 08:19:20 -0600

    adding header for nav bar

--- snap_8_8/snap_8_8.php
+++ snap_8_8/snap_8_8.php
@@ -11,11 +11,17 @@
     <title>Hello, world!</title>
   </head>
   <body>
-	  <nav class="navbar navbar-dark bg-primary">
-		  <!-- Navbar content -->
-	  </nav>
     <h1>Hello, world!</h1>
+<!--	adding header for bootstrap page-->
+	  <header>
+		  <div class="container">
+			  <div class="row">
+				  <div class="col">
 
+				  </div>
+			  </div>
+		  </div>
+	  </header>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
