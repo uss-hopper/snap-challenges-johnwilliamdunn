a6ab9b27aaea677839f43395fd407bcf6a5dcfd5
John Dunn <jdunn33@cnm.edu>
2019-08-22 08:45:52 -0600

    adding 8-22

--- snap_8_20/snap_8_20.js
+++ snap_8_20/snap_8_20.js
@@ -7,8 +7,8 @@
 const ipsum1 = "Ensign Babyface! Your shields were failing, sir. This should be interesting. Your head is not an artifact! What's a knock-out like you doing in a computer-generated gin joint like this? Why don't we just give everybody a promotion and call it a night - 'Commander'? But the probability of making a six is no greater than that of rolling a seven. What? We're not at all alike! I recommend you don't fire until you're within 40,000 kilometers. Wouldn't that bring about chaos? Flair is what marks the difference between artistry and mere competence.";
 
 let newIpsum1 = newIpsum.split(' ');
-let filteredIpsum1 = newIpsum1.filter(!(word => word[0].toLowerCase()=='e'))
-let words = filteredIpsum1.join(' ')
+let filteredIpsum1 = newIpsum1.filter(!(word => word[0].toLowerCase()==='e')),
+let words = filteredIpsum1.join(' ');
 
 
 console.log(words);
---
+++ snap_8_22/snap_8_22.js
@@ -0,0 +1,9 @@
+const array1= [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+console.log(array1.reduce(reducer));
+
+
+let elements = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."];
+
+console.log(elements.join());
