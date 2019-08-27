3952f815a6b8845e9832ba93f877d6900710f937
John Dunn <jdunn33@cnm.edu>
2019-08-27 08:59:36 -0600

    adding new snap challenge

--- snap_8_22/snap_8_22.js
+++ snap_8_22/snap_8_22.js
@@ -3,7 +3,15 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 console.log(array1.reduce(reducer));
 
+let sum = array1.reduce(function(accumulator, currentValue) {
+	return accumulator + currentValue;
+});
 
-let elements = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."];
+console.log(sum);
 
-console.log(elements.join());
+
+const wordArray = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."];
+
+const myReducer = (accumulator, currentvalue) => accumulator + " " + currentvalue;
+
+console.log(wordArray.reduce(myReducer));
---
+++ snap_8_27/snap_8_27.js
@@ -0,0 +1,8 @@
+var numbers = [10, 20, 30, 40] // sums to 100
+var sum = 0;for (var i = 0; i < numbers.length; i++) {
+	sum += numbers[i]
+}
+
+const numbers = [10, 20, 30, 40] // sums to 100// function for adding two numbers. Easy!
+const add = (a, b) =>
+	a + b
